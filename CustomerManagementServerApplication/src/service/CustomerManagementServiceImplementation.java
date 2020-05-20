8c3441b4adbba8327121437c022258c1a7559ffe
johan-new <johan.e.nyberg@icloud.com>
2020-05-20 13:52:48 +0200

    CustomerMngmtSrvImplementation

--- CustomerManagementServerApplication/src/service/CustomerManagementServiceImplementation.java
+++ CustomerManagementServerApplication/src/service/CustomerManagementServiceImplementation.java
@@ -1,5 +1,32 @@
 package service;
+import CustomerManagementService;
+import dao.CustomerDataAccess;
 
+
+@Stateless
 public class CustomerManagementServiceImplementation implements CustomerManagementService {
+
+    @Inject
+    private CustomerDataAccess dao;
+
+    @Override
+    public List<Customer> getAllCustomers() {
+        return dao.findAll();
+    }
+
+    @Override
+    public List<Customer> searchBySurname(String surname) {
+        return dao.find(surname);
+    }
+
+    @Override
+    public void registerCustomer(Customer customer) throws ServiceUnavailableException {
+        dao.add(customer);
+    }
+
+    @Override
+    public removeCustomer(Customer customer){
+        dao.remove(customer);
+    }
 
 }
