ee016c044cdfe271444b21b27311b0f9c0790411
Simon Södergren <simon.sodergren@live.se>
2020-05-20 13:54:17 +0200

    Skrev en förberedande till WebserviceImplementation

--- CustomerManagementServerApplication/src/service/CustomerManagementWebserviceImplementation.java
+++ CustomerManagementServerApplication/src/service/CustomerManagementWebserviceImplementation.java
@@ -1,5 +1,26 @@
 package service;
 
+import java.util.List;
+
+import javax.ejb.Stateless;
+import javax.inject.Inject;
+import javax.jws.WebService;
+
+import domain.Customer;
+
+@Stateless 
+@Webservice(name="CustomerManagementWebservice")
 public class CustomerManagementWebserviceImplementation {
+
+	@Inject 
+	private CustomerManagementServiceLocal service; 
+	
+	public Customer getCustomerById(int id) {
+		return service.getById(id); 
+}
+
+	public List<Customer>getAllCustomer() {
+		return service.getAllCustomer();
+	}
 
 }
