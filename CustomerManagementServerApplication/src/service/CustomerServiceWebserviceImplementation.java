9d1fe788381cb42bcae3885ec63e27ac2c36c1fe
Gitmannen1337 <56875916+Gitmannen1337@users.noreply.github.com>
2020-05-24 16:49:18 +0200

    La till metoder till CustomerServiceWebserviceImplementation samt importerat dessa till klientsidan

--- CustomerManagementServerApplication/src/service/CustomerServiceWebserviceImplementation.java
+++ CustomerManagementServerApplication/src/service/CustomerServiceWebserviceImplementation.java
@@ -9,15 +9,54 @@ import javax.jws.WebService;
 import domain.Customer;
 
 @Stateless
-@WebService(name="CustomerManagementWebService")
+@WebService(name = "CustomerManagementWebService")
 public class CustomerServiceWebserviceImplementation {
-	
-	@Inject 
+
+	@Inject
 	private CustomerManagementServiceLocal service;
-	
+
 	public List<Customer> getAllCustomers() {
 		return service.getAllCustomers();
 	}
-	
-	
+
+	public Customer identifyCustomer(int id) {
+		return service.identifyCustomer(id);
+	}
+
+	public void registerCustomer(Customer customer) {
+		service.registerCustomer(customer);
+	}
+
+	public void removeCustomer(int id) {
+		service.removeCustomer(id);
+	}
+
+	public List<Customer> searchBySurname(String surname) {
+		return service.searchBySurname(surname);
+	}
+
+	public void updateAccountManager(int id, String newAccountManager) {
+		service.updateAccountmanager(id, newAccountManager);
+	}
+
+	public void updateAddress(int id, String newAddress) {
+		service.updateAddress(id, newAddress);
+	}
+
+	public void updateCustomer(int id, String newPhone, String newAddress, String newCustomergroup, String newEmail,
+			String newAccountmanager) {
+		service.updateCustomer(id, newPhone, newAddress, newCustomergroup, newEmail, newAccountmanager);
+	}
+
+	public void updateCustomergroup(int id, String newCustomergroup) {
+		service.updateCustomergroup(id, newCustomergroup);
+	}
+
+	public void updateEmail(int id, String newEmail) {
+		service.updateEmail(id, newEmail);
+	}
+
+	public void updatePhoneNr(int id, String newNr) {
+		service.updatePhoneNr(id, newNr);
+	}
 }
